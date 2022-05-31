f67b621e5e2e414a7ebbb8bcd840496a2e2d3eb0
vinneyto <vinneyto@gmail.com>
2022-05-31 23:37:36 +0300

    change time command

--- src/bot.py
+++ src/bot.py
@@ -33,7 +33,7 @@ def get_text_messages(message):
 
     if message.text == '/time':
         time_utc = dt.datetime.utcnow()
-        bot.reply_to(message, f'Время UTC: {time_utc} My new changes in this file')
+        bot.reply_to(message, f'Время UTC: {time_utc}')
 
     elif message.text.lower() in text_user:
         print(message.text)
